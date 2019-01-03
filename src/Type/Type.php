26e56657659e6b0a6480795a7ee6d29070cac06d
Sandro Keil <github@sandro-keil.de>
2019-01-03 18:11:49 +0100

    It's 2019

--- src/Type/Type.php
+++ src/Type/Type.php
@@ -3,7 +3,7 @@
  * Sandro Keil (https://sandro-keil.de)
  *
  * @link      http://github.com/sandrokeil/arangodb-php-client for the canonical source repository
- * @copyright Copyright (c) 2018 Sandro Keil
+ * @copyright Copyright (c) 2018-2019 Sandro Keil
  * @license   http://github.com/sandrokeil/arangodb-php-client/blob/master/LICENSE.md New BSD License
  */
 
